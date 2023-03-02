6f3a46afd7f14f8ffce84cf1490f170f1a46d5b0
LeonSteinbach <leon@steinba.de>
2023-03-02 03:16:57 +0100

    Can successfully parse the fibonacci example code

--- src/main/scala/de/hfu/monkey/Main.scala
+++ src/main/scala/de/hfu/monkey/Main.scala
@@ -3,7 +3,14 @@ package de.hfu.monkey
 object Main extends App {
 
   private val parser = new Parser()
-  private val result = parser.parseAll(parser.program, "a(1) - b(2);")
+  private val result = parser.parseAll(parser.program,
+    "let fib = fn(n) { " +
+    "  if (n < 2) {" +
+    "    n; " +
+    "  };" +
+    "  fib(n-1) + fib(n-2);" +
+    "};" +
+    "fib(10);")
   println(result)
 
 }
