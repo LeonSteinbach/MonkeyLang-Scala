2ddf981718398650ac8547fca645cbdbaf9ccab7
LeonSteinbach <leon@steinba.de>
2023-03-03 22:20:23 +0100

    Added internal environment

---
+++ src/main/scala/de/hfu/monkey/Environment.scala
@@ -0,0 +1,22 @@
+package de.hfu.monkey
+
+import scala.collection.mutable
+
+class Environment(var outer: Option[Environment] = None) {
+  private val store: mutable.Map[String, Object] = mutable.Map()
+
+  def get(name: String): (Option[Object], Boolean) = {
+    store.get(name) match {
+      case Some(value) => (Some(value), true)
+      case None => outer match {
+        case Some(env) => env.get(name)
+        case None => (None, false)
+      }
+    }
+  }
+
+  def set(name: String, value: Object): Any = {
+    store(name) = value
+    value
+  }
+}
