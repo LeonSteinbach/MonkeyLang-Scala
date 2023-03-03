373e9cc01c505e3e2c8ee65da31a5049aeb9dd40
LeonSteinbach <leon@steinba.de>
2023-03-03 22:21:20 +0100

    Added time measurements and example fibonacci(35) program

--- src/main/scala/de/hfu/monkey/Main.scala
+++ src/main/scala/de/hfu/monkey/Main.scala
@@ -2,9 +2,15 @@ package de.hfu.monkey
 
 object Main extends App {
 
+  private val startTime = System.currentTimeMillis()
   private val parser = new Parser()
-  private val result = parser.parseAll(parser.program,
-    "fn (a, b) { a + b; };")
-  println(result)
+  private val parsed = parser.parseAll(parser.program,
+    "let fibonacci = fn(n) { if (n < 2) { return n; }; fibonacci(n - 1) + fibonacci(n - 2); }; fibonacci(35);")
+  private val evaluated = Evaluator.evaluateProgram(parsed.get, new Environment)
+  private val endTime = System.currentTimeMillis()
+
+  println(parsed)
+  println(evaluated)
+  println(s"Duration [ms]: ${endTime - startTime}")
 
 }
