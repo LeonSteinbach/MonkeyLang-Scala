ba458425f962f86d9f1afafe7b2aa957b31bb75c
LeonSteinbach <leon@steinba.de>
2023-03-11 19:52:57 +0100

    Split node objects to ast.scala

---
+++ src/main/scala/de/hfu/monkey/Ast.scala
@@ -0,0 +1,19 @@
+package de.hfu.monkey
+
+sealed trait Node
+sealed trait Statement extends Node
+sealed trait Expression extends Node
+
+case class Program(statements: List[Statement]) extends Node
+case class Identifier(name: String) extends Expression
+case class IntegerLiteral(value: Int) extends Expression
+case class BooleanLiteral(value: Boolean) extends Expression
+case class LetStatement(name: Identifier, value: Expression) extends Statement
+case class ReturnStatement(value: Expression) extends Statement
+case class BlockStatement(statements: List[Statement]) extends Statement
+case class IfExpression(condition: Expression, consequence: BlockStatement, alternative: BlockStatement) extends Expression
+case class FunctionLiteral(parameters: List[Identifier], body: BlockStatement) extends Expression
+case class CallExpression(function: Expression, arguments: List[Expression]) extends Expression
+case class ExpressionStatement(expression: Expression) extends Statement
+case class PrefixExpression(operator: String, value: Expression) extends Expression
+case class InfixExpression(operator: String, left: Expression, right: Expression) extends Expression
