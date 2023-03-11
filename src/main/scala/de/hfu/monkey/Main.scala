c8dd8c7177c06bcf2af5737a40787f958134d7da
LeonSteinbach <leon@steinba.de>
2023-03-11 19:52:32 +0100

    Optimized parser structure

--- src/main/scala/de/hfu/monkey/Main.scala
+++ src/main/scala/de/hfu/monkey/Main.scala
@@ -2,15 +2,19 @@ package de.hfu.monkey
 
 object Main extends App {
 
-	private val startTime = System.currentTimeMillis()
+	private val startTime1 = System.currentTimeMillis()
 	private val parser = new Parser()
 	private val parsed = parser.parseAll(parser.program,
-		"0123;")
+		"let fib = fn(n) { if (n < 2) { return n; }; fib(n-1) + fib(n-2); }; fib(25);")
+	private val endTime1 = System.currentTimeMillis()
+
+	private val startTime2 = System.currentTimeMillis()
 	private val evaluated = Evaluator.evaluateProgram(parsed.get, new Environment)
-	private val endTime = System.currentTimeMillis()
+	private val endTime2 = System.currentTimeMillis()
 
 	println(parsed)
 	println(evaluated)
-	println(s"Duration [ms]: ${endTime - startTime}")
+	println(s"Parser [ms]:    ${endTime1 - startTime1}")
+	println(s"Evaluator [ms]: ${endTime2 - startTime2}")
 
 }
--- src/main/scala/de/hfu/monkey/Parser.scala
+++ src/main/scala/de/hfu/monkey/Parser.scala
@@ -2,24 +2,6 @@ package de.hfu.monkey
 
 import scala.util.parsing.combinator.RegexParsers
 
-sealed trait Node
-sealed trait Statement extends Node
-sealed trait Expression extends Node
-
-case class Program(statements: List[Statement]) extends Node
-case class Identifier(name: String) extends Expression
-case class IntegerLiteral(value: Int) extends Expression
-case class BooleanLiteral(value: Boolean) extends Expression
-case class LetStatement(name: Identifier, value: Expression) extends Statement
-case class ReturnStatement(value: Expression) extends Statement
-case class BlockStatement(statements: List[Statement]) extends Statement
-case class IfExpression(condition: Expression, consequence: BlockStatement, alternative: BlockStatement) extends Expression
-case class FunctionLiteral(parameters: List[Identifier], body: BlockStatement) extends Expression
-case class CallExpression(function: Expression, arguments: List[Expression]) extends Expression
-case class ExpressionStatement(expression: Expression) extends Statement
-case class PrefixExpression(operator: String, value: Expression) extends Expression
-case class InfixExpression(operator: String, left: Expression, right: Expression) extends Expression
-
 class Parser extends RegexParsers {
 
 	private def identifier: Parser[Identifier] = not("let" | "return" | "fn" | "true" | "false") ~> """[a-zA-Z_]\w*\b""".r ^^ {
@@ -37,12 +19,6 @@ class Parser extends RegexParsers {
 
 	private def value: Parser[Expression] = identifier | integer | boolean
 
-	private def prefixExpression: Parser[PrefixExpression] = ("-" | "!") ~ factor ^^ {
-		case operator ~ value => PrefixExpression(operator, value)
-	}
-
-	private def factor: Parser[Expression] = callExpression | prefixExpression | value | "(" ~> expression <~ ")"
-
 	private def binaryExpression(operatorParser: Parser[String], operandParser: Parser[Expression]): Parser[Expression] = {
 		operandParser ~ rep(operatorParser ~ operandParser) ^^ {
 			case expression ~ list => (list foldLeft expression) {
@@ -51,11 +27,19 @@ class Parser extends RegexParsers {
 		}
 	}
 
+	private def unaryExpression: Parser[PrefixExpression] = ("-" | "!") ~ factor ^^ {
+		case operator ~ value => PrefixExpression(operator, value)
+	}
+
+	private def factor: Parser[Expression] = ifExpression | functionLiteral | callExpression | unaryExpression | value | "(" ~> expression <~ ")"
+
 	private def multiplicativeExpression: Parser[Expression] = binaryExpression("*" | "/", factor)
 
 	private def additiveExpression: Parser[Expression] = binaryExpression("+" | "-", multiplicativeExpression)
 
-	private def expression: Parser[Expression] = ifExpression | functionLiteral | binaryExpression("==" | "!=" | "<" | ">", additiveExpression)
+	private def comparativeExpression: Parser[Expression] = binaryExpression("<" | ">", additiveExpression)
+
+	private def expression: Parser[Expression] = binaryExpression("==" | "!=", comparativeExpression)
 
 	private def expressionStatement: Parser[ExpressionStatement] = expression <~ ";" ^^ {
 		expression => ExpressionStatement(expression)
